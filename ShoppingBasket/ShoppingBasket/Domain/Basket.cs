54e111d45e38aa2f43b2547758d54939eb636086
Werner <werner.who@gmail.com>
2018-08-22 20:33:31 +0100

    Test for when products are added mutliple times, instead of using quantity

--- ShoppingBasket/ShoppingBasket/Domain/Basket.cs
+++ ShoppingBasket/ShoppingBasket/Domain/Basket.cs
@@ -13,19 +13,27 @@ namespace ShoppingBasket.Domain
         {
             _offers = Enumerable.Empty<Offer>();
         }
+
         public Basket(IEnumerable<Offer> offers)
         {
             _offers = offers;
         }
 
         private readonly List<BasketLine> _basketLines = new List<BasketLine>();
-        
+
         public void AddProduct(Product product, int quantity)
         {
-            _basketLines.Add(new BasketLine { Product = product, Quantity = quantity});            
+            _basketLines.Add(new BasketLine {Product = product, Quantity = quantity});
         }
 
-        public decimal Total => _basketLines.Sum(bl => bl.Quantity * bl.Product.Price) - _offers.Sum(o => o.CalculateDiscount(_basketLines));
-   
-    }
+        public decimal Total {
+            get
+            {
+                var combinedBasketLines = _basketLines.GroupBy(bl => bl.Product.Id).Select( g => new BasketLine{ Product = g.First().Product, Quantity = g.Sum( x=> x.Quantity  )});
+                return _basketLines.Sum(bl => bl.Quantity * bl.Product.Price) - _offers.Sum(
+                    o => o.CalculateDiscount(combinedBasketLines));
+            }
+        }
+
+}
 }
--- ShoppingBasket/ShoppingBasketTests/Domain/BasketTests.cs
+++ ShoppingBasket/ShoppingBasketTests/Domain/BasketTests.cs
@@ -57,7 +57,7 @@ namespace ShoppingBasketTests.Domain
         public void When4MilkAreInTheBasket_BasketShouldApplyDiscountAndReturnCorrectPrice()
         {
             var sut = new Basket(new List<Offer>{OfferRepositoryMock.ButterOffer, OfferRepositoryMock.MilkOffer});
-            //Given the basket has 4 butter
+            //Given the basket has 4 Milk
             sut.AddProduct(ProductRepositoryMock.Milk, 4);
             //The total should be 3.45
             Assert.Equal(3.45m, sut.Total);
@@ -73,6 +73,19 @@ namespace ShoppingBasketTests.Domain
             sut.AddProduct(ProductRepositoryMock.Milk,8);
             //the total should be £9.00
             Assert.Equal(9m, sut.Total);
+        }
+
+        [Fact]
+        public void WhenProductsAddedMultipleTimes_DiscountIsCalculatedCorrectly()
+        {
+            var sut = new Basket(new List<Offer> { OfferRepositoryMock.ButterOffer, OfferRepositoryMock.MilkOffer });
+            //Given the basket contains 4 milk
+            sut.AddProduct(ProductRepositoryMock.Milk, 1);
+            sut.AddProduct(ProductRepositoryMock.Milk, 1);
+            sut.AddProduct(ProductRepositoryMock.Milk, 1);
+            sut.AddProduct(ProductRepositoryMock.Milk, 1);
+            //The total should be 3.45
+            Assert.Equal(3.45m, sut.Total);
         }
     }
 }
