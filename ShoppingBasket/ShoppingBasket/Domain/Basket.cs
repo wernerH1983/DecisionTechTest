5285b8cfdce8a6b1e79c2438564ef37e8ab14bcc
Werner <werner.who@gmail.com>
2018-08-22 18:57:12 +0100

    Store Product and quantity in the basket ( in basketline) Calculate correct price

--- ShoppingBasket/ShoppingBasket/Domain/Basket.cs
+++ ShoppingBasket/ShoppingBasket/Domain/Basket.cs
@@ -1,20 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ShoppingBasket.Domain
 {
     public class Basket
     {
+        private readonly List<BasketLine> _basketLines = new List<BasketLine>();
         
         public void AddProduct(Product product, int quantity)
         {
-            
+            _basketLines.Add(new BasketLine { Product = product, Quantity = quantity});            
         }
 
-        public decimal Total
-        {
-            get { return 0m; }
-        }
+        public decimal Total => _basketLines.Sum(bl => bl.Quantity * bl.Product.Price);
+
+
+
     }
 }
---
+++ ShoppingBasket/ShoppingBasket/Domain/BasketLine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingBasket.Domain
+{
+    public class BasketLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
